b73443d4d80c531b71907b938f7d22dc752e9026
Kevin Hu <kevinhuwest@gmail.com>
2020-07-23 15:27:58 -0400

    Processed TCGA whitelisted copy number

--- cancer_data/process.py
+++ cancer_data/process.py
@@ -729,6 +729,14 @@ class Processors:
 
         return df
 
+    def tcga_cn_whitelisted(raw_path):
+
+        df = pd.read_csv(raw_path, sep="\t", index_col=0)
+        df = df.T
+        df = df.astype(np.float16)
+
+        return df
+
 
 if __name__ == "__main__":
 
