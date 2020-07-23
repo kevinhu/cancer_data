6cfc92884805928ed930677bb2a514eb155560ab
Kevin Hu <kevinhuwest@gmail.com>
2020-07-23 15:14:46 -0400

    Processed TCGA continuous copy number

--- cancer_data/process.py
+++ cancer_data/process.py
@@ -713,6 +713,14 @@ class Processors:
 
         return df
 
+    def tcga_cn_continuous(raw_path):
+
+        df = pd.read_csv(raw_path, sep="\t", index_col=0)
+        df = df.T
+        df = df.astype(np.float16)
+
+        return df
+
 
 if __name__ == "__main__":
 
