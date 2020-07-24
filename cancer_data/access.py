1a790e80a93088480e00877887aee1f5de4bf6f6
Kevin Hu <kevinhuwest@gmail.com>
2020-07-23 22:46:14 -0400

    Fix tab/space inconsistencies

--- cancer_data/access.py
+++ cancer_data/access.py
@@ -5,70 +5,70 @@ import pandas as pd
 
 class Datasets:
 
-	def load(dataset_id, **kwargs):
+    def load(dataset_id, **kwargs):
 
-		dataset_path = PROCESSED_DIR / f"{dataset_id}.h5"
+        dataset_path = PROCESSED_DIR / f"{dataset_id}.h5"
 
-		assert dataset_id in SCHEMA.index, f"{dataset_id} not in schema!"
-		assert file_exists(dataset_path), f"{dataset_id} does not exist!"
+        assert dataset_id in SCHEMA.index, f"{dataset_id} not in schema!"
+        assert file_exists(dataset_path), f"{dataset_id} does not exist!"
 
-		df = pd.read_hdf(dataset_path, **kwargs)
+        df = pd.read_hdf(dataset_path, **kwargs)
 
-		return df
+        return df
 
-	def description(dataset_id):
+    def description(dataset_id):
 
-		assert dataset_id in SCHEMA.index, f"{dataset_id} not in schema!"
+        assert dataset_id in SCHEMA.index, f"{dataset_id} not in schema!"
 
-		return SCHEMA.loc[dataset_id, "description"]
+        return SCHEMA.loc[dataset_id, "description"]
 
-	def summary(dataset_id):
+    def summary(dataset_id):
 
-		print(SCHEMA.index)
+        print(SCHEMA.index)
 
-		assert dataset_id in SCHEMA.index, f"{dataset_id} not in schema!"
+        assert dataset_id in SCHEMA.index, f"{dataset_id} not in schema!"
 
-		dataset_row = SCHEMA.loc[dataset_id]
+        dataset_row = SCHEMA.loc[dataset_id]
 
-		if dataset_row["type"] == "reference":
+        if dataset_row["type"] == "reference":
 
-			dataset_summary = "\n".join([
-				f"ID: {dataset_row['id']}",
-				f"Type: {dataset_row['type']}",
-				f"Description: {dataset_row['description']}",
-				f"Provider: {dataset_row['source']}",
-				f"Source URL: {dataset_row['url']}",
-				f"Portal URL: {dataset_row['portal_url']}",
-				f"Size: {dataset_row['downloaded_size']}",
-				f"md5sum: {dataset_row['downloaded_md5']}"
-			])
+            dataset_summary = "\n".join([
+                f"ID: {dataset_row['id']}",
+                f"Type: {dataset_row['type']}",
+                f"Description: {dataset_row['description']}",
+                f"Provider: {dataset_row['source']}",
+                f"Source URL: {dataset_row['url']}",
+                f"Portal URL: {dataset_row['portal_url']}",
+                f"Size: {dataset_row['downloaded_size']}",
+                f"md5sum: {dataset_row['downloaded_md5']}"
+            ])
 
-		elif dataset_row["type"] == "primary_dataset":
+        elif dataset_row["type"] == "primary_dataset":
 
-			dataset_summary = "\n".join([
-				f"ID: {dataset_row['id']}",
-				f"Type: {dataset_row['type']}",
-				f"Dependencies: {dataset_row['dependencies']}",
-				f"Description: {dataset_row['description']}",
-				f"Provider: {dataset_row['source']}",
-				f"Source URL: {dataset_row['url']}",
-				f"Portal URL: {dataset_row['portal_url']}",
-				f"Size: {dataset_row['downloaded_size']}",
-				f"md5sum: {dataset_row['downloaded_md5']}"
-			])
+            dataset_summary = "\n".join([
+                f"ID: {dataset_row['id']}",
+                f"Type: {dataset_row['type']}",
+                f"Dependencies: {dataset_row['dependencies']}",
+                f"Description: {dataset_row['description']}",
+                f"Provider: {dataset_row['source']}",
+                f"Source URL: {dataset_row['url']}",
+                f"Portal URL: {dataset_row['portal_url']}",
+                f"Size: {dataset_row['downloaded_size']}",
+                f"md5sum: {dataset_row['downloaded_md5']}"
+            ])
 
-		elif dataset_row["type"] == "secondary_dataset":
+        elif dataset_row["type"] == "secondary_dataset":
 
-			dataset_summary = "\n".join([
-				f"ID: {dataset_row['id']}",
-				f"Type: {dataset_row['type']}",
-				f"Dependencies: {dataset_row['dependencies']}",
-				f"Description: {dataset_row['description']}",
-				f"Provider: {dataset_row['source']}",
-				f"Source URL: {dataset_row['url']}",
-				f"Portal URL: {dataset_row['portal_url']}",
-				f"Size: {dataset_row['downloaded_size']}",
-				f"md5sum: {dataset_row['downloaded_md5']}"
-			])
+            dataset_summary = "\n".join([
+                f"ID: {dataset_row['id']}",
+                f"Type: {dataset_row['type']}",
+                f"Dependencies: {dataset_row['dependencies']}",
+                f"Description: {dataset_row['description']}",
+                f"Provider: {dataset_row['source']}",
+                f"Source URL: {dataset_row['url']}",
+                f"Portal URL: {dataset_row['portal_url']}",
+                f"Size: {dataset_row['downloaded_size']}",
+                f"md5sum: {dataset_row['downloaded_md5']}"
+            ])
 
-		return dataset_summary
+        return dataset_summary
--- cancer_data/download.py
+++ cancer_data/download.py
@@ -4,19 +4,19 @@ from utils import download_from_url
 
 if __name__ == "__main__":
 
-	for _, file in SCHEMA.iterrows():
+    for _, file in SCHEMA.iterrows():
 
-		if file["type"] == "reference":
+        if file["type"] == "reference":
 
-			download_from_url(
-			    file["url"],
-			    f"{REFERENCE_DIR}/{file['downloaded_name']}",
-			    reference_md5 = file['downloaded_md5']
-			)
+            download_from_url(
+                file["url"],
+                f"{REFERENCE_DIR}/{file['downloaded_name']}",
+                reference_md5 = file['downloaded_md5']
+            )
 
-		elif file["type"] == "primary_dataset":
-			download_from_url(
-			    file["url"],
-			    f"{DOWNLOAD_DIR}/{file['downloaded_name']}",
-			    reference_md5 = file['downloaded_md5']
-			)
+        elif file["type"] == "primary_dataset":
+            download_from_url(
+                file["url"],
+                f"{DOWNLOAD_DIR}/{file['downloaded_name']}",
+                reference_md5 = file['downloaded_md5']
+            )
