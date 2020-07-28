3c9ef4fcc5916d3364a75089665adc72b94b4487
Kevin Hu <kevinhuwest@gmail.com>
2020-07-27 23:36:05 -0400

    Added CCLE chromatin profiling to schema

--- test.py
+++ test.py
@@ -1,5 +1,3 @@
 import cancer_data
 
-cancer_data.download_and_process("ccle_annotations", process_kwargs={"overwrite": True,"delete_raw":True})
-
-print(cancer_data.summary("ccle_annotations"))
+cancer_data.process("tcga_mx")
